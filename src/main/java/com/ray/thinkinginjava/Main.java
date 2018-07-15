8950c75e1bf9b52dae7d5f3355cbbad1ad480974
Ray Li <34641656+RayRuiLi@users.noreply.github.com>
2018-07-15 14:50:39 +0800

    Upload code

---
+++ src/main/java/com/ray/thinkinginjava/Main.java
@@ -0,0 +1,11 @@
+package com.ray.thinkinginjava;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        //Empty Main
+//        Collection
+    }
+}
